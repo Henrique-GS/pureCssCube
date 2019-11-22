89b415946a928078b7a120b0fa73ada9c6ff21ad
Henrique Godez Silva <henrique.silva@easynvest.com.br>
2019-11-22 19:37:38 -0300

    Fix some (not all) bad code practices

--- index.js
+++ index.js
@@ -1,87 +1,80 @@
 (function IIFE(){
     "use strict"
     window.addEventListener("mousemove", (event) => rotateCube(event))
-
+    
     const w = window.innerWidth/360;
     const h = window.innerHeight/360;
-
+    
     const rotateCube = (event) => {
         let x = event.clientX/w;
         let y = event.clientY/h;
-
+        
         let cube = document.querySelector("#cube")
-
+        
         cube.style.transform = `rotateX(${y}deg) rotateZ(${x}deg) translateY(-3em)`
         console.log("x",x,"y",y)
     }
-
-
-
-
+    
+    
+    
+    
     //Triangles:
-
-    const bola = document.querySelector('#bola')
-    const bola2 = document.querySelector('#bola2')
-
+    
+    const innerRing = document.querySelector('#innerRing')
+    const outterRing = document.querySelector('#outterRing')
+    const fullRing = document.querySelector('#fullRing')
+    
     const glo = 15;
     const mul = 100;
     let isup = true
-
-    const row = document.createElement('div')
+    
+    
+    const fillTriangleRing = (parentElement = null, radiusScale = 1, invertGradient = false, fillRing = false) => {
+        
+        if(!parentElement) return
+        
+        const newMul = fillRing ? 3.75 : mul
+        const row = document.createElement('div')
         row.classList.add('row')
-
-        for(let i = 0; i<360; i+=mul){
+        
+        for(let i = 0; i<=360; i+= newMul){
+            
+            const currentGrayValue = invertGradient ? i+10 : 360-i+10
+            const opacity = invertGradient ? .5 : 1
             
             isup = !isup
-            let x = Math.cos(i*Math.PI/180)*glo
-            let z = Math.sin(i*Math.PI/180)*glo
-            console.log(x,z)
-
-            const tri = document.createElement('div');
+            let x = Math.cos(i*Math.PI/180)*glo*radiusScale
+            let z = Math.sin(i*Math.PI/180)*glo*radiusScale
+            
+            const tri = document.createElement('div')
+            tri.classList.add('tri')
             tri.classList.add(isup?'triup':'tridown')
-            tri.style.transform = `translateX(${x}em) translateZ(${z}em) rotateY(${90-i}deg)`
-            isup ? tri.style.borderBottom = `4.33em solid rgb(${i+10},${i+10},${i+10})`
-            : tri.style.borderTop = `4.33em solid rgb(${i+10},${i+10},${i+10})`
-            tri.style.filter = "sepia(100%)"
+            fillRing && tri.classList.add('filled')
+
+            tri.style.transform = `translateX(${x+1.5}em) translateZ(${z}em) rotateY(${90-i}deg)`
+            isup ? tri.style.borderBottomColor = `rgba(${currentGrayValue},${currentGrayValue},${currentGrayValue},${opacity})`
+            : tri.style.borderTopColor = `rgba(${currentGrayValue},${currentGrayValue},${currentGrayValue},${opacity})`
+
             row.appendChild(tri)
         }
-        bola.appendChild(row)
-
-        const row2 = document.createElement('div')
-        row2.classList.add('row2')
-
-        for(let i = 0; i<360; i+=mul){
-            
-            isup = !isup
-            let x = Math.cos(i*Math.PI/180)*glo*1.5
-            let z = Math.sin(i*Math.PI/180)*glo*1.5
-            console.log(x,z)
-
-            const tri = document.createElement('div');
-            tri.classList.add(isup?'triup':'tridown')
-            tri.style.transform = `translateX(${x}em) translateZ(${z}em) rotateY(${90-i}deg)`
-            isup ? tri.style.borderBottom = `4.33em solid rgba(${360-i+10},${360-i+10},${360-i+10},.5)`
-            : tri.style.borderTop = `4.33em solid rgba(${360-i+10},${360-i+10},${360-i+10},.5)`
-            tri.style.filter = "sepia(100%)"
-            row2.appendChild(tri)
-        }
-        bola2.appendChild(row2)
-
-
-    const rotateBola = (event) => {
+        parentElement.appendChild(row)
+    }
+    
+    fillTriangleRing(innerRing)
+    fillTriangleRing(fullRing, 1.25, true, true)
+    fillTriangleRing(outterRing, 1.5, true, false)
+    
+    
+    const rotateinnerRing = (event) => {
         let x = event.clientX/w;
         let y = event.clientY/h;
-
-        let bola = document.querySelector("#bola")
-
-        bola.style.transform = `rotateX(${y}deg) rotateZ(${x}deg)`
-
-        let bola2 = document.querySelector("#bola2")
-
-        bola2.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`
+        
+        innerRing.style.transform = `rotateX(${y}deg) rotateZ(${x}deg)`
+        
+        outterRing.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`
     }
-
     
-    window.addEventListener("mousemove", (event) => rotateBola(event))
-
+    
+    window.addEventListener("mousemove", (event) => rotateinnerRing(event))
+    
 })()
